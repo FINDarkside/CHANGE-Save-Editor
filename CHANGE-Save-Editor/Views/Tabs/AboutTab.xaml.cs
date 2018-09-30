731a83dcc2548d0fbeca20204c57557f4863b8b0
FINDarkside <findarkside@gmail.com>
2018-09-30 18:48:20 +0300

    Add about tab

---
+++ CHANGE-Save-Editor/Views/Tabs/AboutTab.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CHANGE_Save_Editor.Views.Tabs
+{
+
+    public partial class AboutTab : UserControl
+    {
+        public AboutTab()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewOnGitHubClicked(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/FINDarkside/CHANGE-Save-Editor");
+        }
+    }
+}
